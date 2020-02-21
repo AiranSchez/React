06132c89da376d1edd86c564ded8cf9df978d38b
2dawa <2dawa@informatica.edu>
2020-02-21 10:02:31 +0000

    Añadida selección de personajes aleatoria

---
+++ js/conexion.js
@@ -0,0 +1,21 @@
+
+let datos;
+
+let conexion = new XMLHttpRequest();
+
+addEventListener('load', inicializarEventos, false);
+
+function inicializarEventos() {
+    conexion.onreadystatechange = procesarEventos;
+    conexion.open("GET", "http://ddragon.leagueoflegends.com/cdn/10.4.1/data/es_ES/champion.json", true);
+    conexion.send();
+    
+
+}
+
+function procesarEventos() {
+    if (conexion.readyState == 4) {
+        datos = JSON.parse(conexion.responseText);
+    }
+
+}
