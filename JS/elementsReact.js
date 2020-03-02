79c649601130c1b6a1d3c77283966b00a48d6b6a
2dawa <2dawa@informatica.edu>
2020-03-02 12:10:13 +0000

    Añadidas imágenes de todos los personajes

--- JS/elementsReact.js
+++ JS/elementsReact.js
@@ -1,56 +1,62 @@
-function when_external_loaded(callback) {
+function when_external_loaded (callback) {
   if (typeof datos === 'undefined') {
-    setTimeout(function () {
-      when_external_loaded(callback);
+    setTimeout (function () {
+       when_external_loaded (callback);
     }, 100); // wait 100 ms
-  } else { callback(); }
+  } else { callback (); }
 }
 
-when_external_loaded(function () {
-  let nombres = [];
-  for (let x in datos.data) {
+when_external_loaded (function () {
+let nombres=[];
+for (let x in datos.data) {
     nombres.push(x);
-  }
-  let elegidosIzquierda = [];
-  elegir(elegidosIzquierda, nombres);
-  let elegidosDerecha = [];
-  elegir(elegidosDerecha, nombres);
-  for (let i = 0; i < elegidosIzquierda.length; i++) {
-    let idizquierda = 'izquierda' + (i + 1);
-    let idderecha = 'derecha' + (i + 1);
-    ReactDOM.render(<Campeon name={elegidosIzquierda[i]} />, document.getElementById(idizquierda));
-    ReactDOM.render(<Campeon name={elegidosDerecha[i]} />, document.getElementById(idderecha));
-  }
-  //
-  //Modificación para intentar poner todos los personajes en el centro
-  for (let j = 0; j < array.length; j++) {
-    
-    
-  }
-  //
+  }  
 
+let elegidosIzquierda=[];
+elegir(elegidosIzquierda,nombres);
+let elegidosDerecha=[];
+elegir(elegidosDerecha,nombres);
+
+for(let i=0;i<nombres.length;i++){
+ let elemento=document.createElement('div');     
+ elemento.setAttribute('id',i);
+ document.getElementById('campeones').append(elemento);
+  ReactDOM.render(<CampeonImagen name={nombres[i]}/>,document.getElementById(i));      
+  let idizquierda = 'izquierda' + (i + 1);
+  let idderecha = 'derecha' + (i + 1);
+  if(i<5){
+  ReactDOM.render(<Campeon name={elegidosIzquierda[i]} />, document.getElementById(idizquierda));
+  ReactDOM.render(<Campeon name={elegidosDerecha[i]} />, document.getElementById(idderecha));
+
+  }
+  
+}
+  
 });
 
-function elegir(elegidos, nombres) {
-  while (elegidos.length < 5) {
+function elegir(elegidos,nombres){
+while (elegidos.length < 5) {
     let number = Math.floor((Math.random() * nombres.length));
     if (!elegidos.includes(nombres[number])) {
-      elegidos.push(nombres[number]);
+        elegidos.push(nombres[number]);
     }
   }
 }
+function Campeon(props){
 
-function Campeon(props) {
-
-  let img = "http://ddragon.leagueoflegends.com/cdn/10.4.1/img/champion/" + datos.data[props.name].image.full;
-  console.log(datos.data[props.name])
+  let img="http://ddragon.leagueoflegends.com/cdn/10.4.1/img/champion/"+datos.data[props.name].image.full;
   return (
-    <div>
-      <img src={img} width="100"/>
-      <div className="descripcion">
-        <p id="name">{datos.data[props.name].name}</p>
-        <p id="title">{datos.data[props.name].title}</p>
-      </div>
-    </div>
-  )
+      <div >
+        <img src={img}/>
+        <p>{datos.data[props.name].name}</p>  
+      </div>   
+      )
+}
+
+function CampeonImagen(props){
+
+let img="http://ddragon.leagueoflegends.com/cdn/10.4.1/img/champion/"+datos.data[props.name].image.full;
+return (
+      <img src={img} width='100'/>
+    )
 }
